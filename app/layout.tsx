b72cfd5ce71f3f7ea1afe55a0be2fb9f56128217
Hasan Şişik <sisik.hsn@gmail.com>
2026-04-28 18:56:47 +0300

    vercel analiz

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
 };
 
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -33,6 +34,7 @@ export default function RootLayout({
         <Providers>
           {children}
           <Toaster position="bottom-center" />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
